ffdecbb7e0f1a04747b48a5d8c203c45c687ca5b
mohammedsalem <gg.goo.mobile@gmail.com>
2024-03-03 21:59:56 +0300

    add jitpack.yml

--- src/main/java/com/libb/LibBClass.java
+++ src/main/java/com/libb/LibBClass.java
@@ -1,9 +1,11 @@
+package com.libb;
+
 public class LibBClass {
 
     public int fun0(){
         return 20;
     }
     public static void fun01() {
-        System.out.println("LibBClass.");
+        System.out.println("com.libb.LibBClass.");
     }
 }
